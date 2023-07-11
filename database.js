d503609f0b4d17ddfdac8b21781a6183d5a2b890
Samr Mouna <samr.mouna@gmail.com>
2023-07-11 14:59:38 -0400

    pools local database and create table for it

--- database.js
+++ database.js
@@ -9,4 +9,30 @@ const pool = new Pool({
   port: process.env.DB_PORT
 });
 
-module.exports = pool;
+const createBookTableQuery = `
+  CREATE TABLE IF NOT EXISTS book_inventory (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    author VARCHAR(50) NOT NULL,
+    genre VARCHAR(50) NOT NULL,
+    quantity INTEGER NOT NULL
+  );
+`;
+
+const createBookTable = async () => {
+  try {
+    await pool.query(createBookTableQuery);
+    console.log('Book Table Created');
+  } catch (err) {
+    console.error('Error executing query, err.stack');
+  }
+} 
+
+createBookTable();
+
+module.exports = {
+  query: (text, params, callback) => {
+    console.log("QUERY:", text, params || "");
+    return pool.query(text, params, callback);
+  }
+};
