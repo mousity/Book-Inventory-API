555cb7bdb33757eba2b5f8f51c553f247d0f1930
Samr Mouna <samr.mouna@gmail.com>
2023-07-11 15:10:01 -0400

    added export statement

--- books.js
+++ books.js
@@ -2,8 +2,17 @@ const books = [
     {
         id: 1,
         title: "sample",
-        author: "sampleauth",
+        author: "sample auth",
+        genre: "sample genre",
+        quantity: 1
+    },
+    {
+        id: 2,
+        title: "sample",
+        author: "sample auth",
         genre: "sample genre",
         quantity: 1
     }
 ]
+
+module.exports = books;
